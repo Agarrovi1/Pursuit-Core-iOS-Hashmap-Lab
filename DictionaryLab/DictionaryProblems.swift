2caef482a2ff82a3bd09fd426d955ee9048dfe8d
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2019-09-25 17:08:51 -0400

    answers 2,3

--- DictionaryLab/DictionaryProblems.swift
+++ DictionaryLab/DictionaryProblems.swift
@@ -25,13 +25,49 @@ func mostFrequentlyOccurringChar(in str: String) -> Character {
 // Find the first indicies whose values sum to a given number
 
 func pairSum(arr: [Int], target: Int) -> (Int, Int) {
+    var indexAndNum = [Int:Int]()
+    for (index,value) in arr.enumerated() {
+        indexAndNum[index] = value
+    }
+    for a in 0...arr.count - 1 {
+        for (index,value) in indexAndNum {
+            if a == index {
+                continue
+            }
+            if arr[a] + value == target {
+                return (a,index)
+            }
+        }
+    }
     return (0, 0)
 }
 
 // Find the second most frequently occurring character in a given string
 
 func secondMostFrequentlyOccurringChar(in str: String) -> Character {
-    return "b"
+    let mostFreqChar = mostFrequentlyOccurringChar(in: str)
+    var frequencyDict = [Character:Int]()
+    for a in str {
+        if let freq = frequencyDict[a] {
+            frequencyDict[a] = freq + 1
+        } else {
+            frequencyDict[a] = 1
+        }
+    }
+    var secondHighestCount = 0
+    var answer = Character(" ")
+    for (char,freq) in frequencyDict {
+        if char == mostFreqChar {
+            continue
+        }
+        if secondHighestCount < freq {
+            answer = char
+            secondHighestCount = freq
+        }
+        
+    }
+    
+    return answer
 }
 
 // https://leetcode.com/problems/ransom-note/
